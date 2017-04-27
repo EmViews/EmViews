2ff10cfc98b04f44f84d0d935ae4a17287bbbb7c
John Nguyen <John Nguyen>
2017-04-27 12:11:47 -0700

    4/27/17 @ 12:11pm (initial design)

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+	apiKey: "AIzaSyDBcUj9XDES0jIESlddtCodAWt8pWCfsLA",
+	authDomain: "john-57aba.firebaseapp.com",
+	databaseURL: "https://john-57aba.firebaseio.com",
+	projectId: "john-57aba",
+	storageBucket: "john-57aba.appspot.com",
+	messagingSenderId: "309633750648"
+	};
+
+firebase.initializeApp(config);
+
