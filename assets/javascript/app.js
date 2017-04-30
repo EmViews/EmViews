c40f466bf2ea01bcc2795c0fdbf635ebd461802b
Cullan Shewfelt <cullanshewfelt@github.com>
2017-04-29 18:06:21 -0700

    settling merge conflicts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,68 +13,68 @@ $(document).ready(function() {
         // -----------------END OF FIREBASE KEY------------------
 
         // UI modification
-        $("#navbarTop").hide();
-        $("#searchResultsFirst").hide();
-        $("#searchResultsSecond").hide();
-        $("#mainBody").hide();
+        $("#navbar-top").hide();
+        $("#search-results-first").hide();
+        $("#search-results-second").hide();
+        $("#main-body").hide();
         $("#videos").hide();
         $("#reddit-row").hide();
         $("#youtubes").hide();
 
         $(document).on('click', "#submit-button2", function() {
-            $("#navbarStart").hide(1000);
-            $("#navbarTop").show(1000);
-            $("#searchResultsFirst").show(1000);
+            $("#navbar-start").hide(1000);
+            $("#navbar-top").show(1000);
+            $("#search-results-first").show(1000);
         });
 
-        $(document).on('click', '#searchResultsFirst', function() {
+        $(document).on('click', '#search-results-first', function() {
             event.preventDefault();
-            $("#searchResultsFirst").hide(750);
-            $("#searchResultsSecond").show(750);
-            $("#mainBody").show(1000);
+            $("#search-results-first").hide(750);
+            $("#search-results-second").show(750);
+            $("#main-body").show(1000);
             $("#videos").show(1000);
         });
 
         $(document).on('click', '#submit-button1', function() {
             event.preventDefault();
-            $("#searchResultsSecond").hide(1000);
-            $("#mainBody").hide(750);
+            $("#search-results-second").hide(1000);
+            $("#main-body").hide(750);
             $("#videos").hide(750);
-            $("#searchResultsFirst").show(1000);
+            $("#search-results-first").show(1000);
         });
-        $(document).on("click", "#productBtn", function() {
+        $(document).on("click", "#product-btn", function() {
 
             event.preventDefault();
-            $("#pictures").show(1000);
-            $("#specifications").show(1000);
+            $("#picture-div").show(1000);
+            $("#specs-div").show(1000);
             $("#reddit-row").hide(1000);
             $("#comments").show(1000);
             $("#youtubes").hide(1000);
-            $("#productBtn").addClass("active");
-            $("#redditBtn").removeClass("active")
-            $("#youtubeBtn").removeClass("active");
+            $("#product-btn").addClass("active");
+            $("#reddit-btn").removeClass("active")
+            $("#youtube-btn").removeClass("active");
         })
-        $(document).on("click", "#redditBtn", function() {
+        $(document).on("click", "#reddit-btn", function() {
             event.preventDefault();
-            $("#pictures").hide(1000);
-            $("#specifications").hide(1000);
+            $("#picture-div").hide(1000);
+            $("#specs-div").hide(1000);
             $("#reddit-row").show(750);
             $("#comments").hide(1000);
             $("#youtubes").hide(750);
-            $("#productBtn").removeClass("active");
-            $("#redditBtn").addClass("active")
-            $("#youtubeBtn").removeClass("active");
+            $("#product-btn").removeClass("active");
+            $("#reddit-btn").addClass("active")
+            $("#youtube-btn").removeClass("active");
         });
-        $(document).on("click", "#youtubeBtn", function() {
+        $(document).on("click", "#youtube-btn", function() {
             event.preventDefault();
-            $("#pictures").hide(1000);
-            $("#specifications").hide(1000);
+            $("#picture-div").hide(1000);
+            $("#specs-div").hide(1000);
             $("#reddit-row").hide(750);
             $("#comments").hide(1000);
             $("#youtubes").show(900);
-            $("#productBtn").removeClass("active");
-            $("#redditBtn").removeClass("active")
-            $("#youtubeBtn").addClass("active");
+            $("#product-btn").removeClass("active");
+            $("#reddit-btn").removeClass("active")
+            $("#youtube-btn").addClass("active");
         });
     })
     // -----------------END OF DOCUMENT READY ---------------
