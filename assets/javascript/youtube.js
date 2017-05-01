282cc26148e11acbe8de749567b1bfc34c8f4a65
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-05-01 00:23:58 -0700

    Youtube API final updates

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,43 +1,22 @@
-function tplawesome(e, t) { res = e;
-    for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function(e, r) {
-            return t[n][r] }) }
-    return res }
+$(document).on('click', '.search-button', function() {
+    event.preventDefault();
+    searchQuery = encodeURIComponent($("#product-input").val() + " review").replace(/%20/g, "+");
+    $("#youtube-results").empty();
+    queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=viewCount&q=" + searchQuery + "&type=video&videoDefinition=high&key=AIzaSyDgExUpyuvNlKbGk8O27hoNQISlT_9huuk"
 
-$(function() {
-    $(document).on("click", ".search-button", function() {
-        event.preventDefault();
-        var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#product-input").val()+"review").replace(/%20/g, "+"),
-            maxResults: 5,
-            order: "viewCount",
-            publishedAfter: "2000-01-01T00:00:00Z"
-        });
-
-        request.execute(function(response) {
-            var results = response.result;
-            $.each(results.items, function(index, item) {
-                $.get("tpl/item.html", function(data) {
-                    $("#youtube-results").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
-                });
-            });
-        })
-    });
-});
-
-function init() {
-    gapi.client.setApiKey("AIzaSyDgExUpyuvNlKbGk8O27hoNQISlT_9huuk");
-    gapi.client.load("youtube", "v3", function() {
-    });
-};
-
-// queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=viewCount&q=puppy&type=video&videoDefinition=high&key={AIzaSyDgExUpyuvNlKbGk8O27hoNQISlT_9huuk}"
-  
-  
- // $.ajax({
-//           url: queryURL,
- //           method: "GET"
- //         })
- //         // After the data comes back from the API
- //         .done(function(response) { 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        var items = response.items;
+        for (var i = 0; i < items.length; i++) {
+            items[i]
+            var element = $("<div class='items'>")
+            var title = $("<h2>").html(items[i].snippet.title);
+            var video = $(`<iframe class='video w100' width='854' height='480' src='//www.youtube.com/embed/${items[i].id.videoId}' frameborder='0' allowfullscreen></iframe>`);
+            element.append(title);
+            element.append(video);
+            $("#youtube-results").append(element);
+        }
+    })
+})
